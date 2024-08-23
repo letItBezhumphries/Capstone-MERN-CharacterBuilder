6592741d7ab0336ebf2312d1ab6ba5ca15efcdf7
Eric Humphries <erichumphries@Erics-MBP.lan>
2024-08-23 16:15:39 -0700

    fixed the express server to run in development mode and to serve the static application code on any request

--- backend/index.js
+++ backend/index.js
@@ -1,18 +1,18 @@
 const express = require('express');
+
 const path = require('path');
-const bodyParser = require('body-parser');
+// const bodyParser = require('body-parser');
 require('dotenv').config();
 const PORT = process.env.PORT;
-const PUBLIC_DIR = path.join(__dirname, 'public');
 const app = express();
-const { notFound, errorHandler } = require('./middleware/errorMiddleware');
+const morgan = require('morgan');
 
 const connectDB = require('./db/index');
 
 connectDB();
 
-/* models for home page */
-const Monster = require('./models/Monster');
+/* Middleware imports */
+const { notFound, errorHandler } = require('./middleware/errorMiddleware');
 
 /** define Routes */
 const authRoutes = require('./routes/auth');
@@ -21,12 +21,17 @@ const characterRoutes = require('./routes/characters');
 const dungeonRoutes = require('./routes/dungeons');
 const monsterRoutes = require('./routes/monsters');
 
+/** Middleware */
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(express.json());
 
-/** Wiring in route handlers */
+/** Wiring in route handlers and mounting routes */
 app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/characters', characterRoutes);
@@ -35,34 +40,29 @@ app.use('/api/monsters', monsterRoutes);
 // set up middleware
 
 // serve static files
-app.use('public', express.static(PUBLIC_DIR));
+// app.use('public', express.static(PUBLIC_DIR));
 
-// serve up the home page
-app.get('/', async (req, res, next) => {
-  try {
-    const monsters = await Monster.find({});
+const DIR_NAME = path.resolve();
 
-    if (monsters) {
-      res.status(200).render('index', {
-        page_title: 'Welcome to the D&D Dungeon Builder',
-        monstersView: true,
-        monsters,
-        authenticated: false,
-      });
-    } else {
-      next();
-    }
-  } catch (error) {
-    console.error('error:', error);
-  }
-});
+// serve up the application
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(DIR_NAME, '/frontend/dist')));
 
-app.set('view engine', 'pug');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(DIR_NAME, 'frontend', 'dist', 'index.html'));
+  });
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running...');
+  });
+}
 
 /* handle any requests that fall through */
 app.use(notFound);
 app.use(errorHandler);
 
 app.listen(PORT, () => {
-  console.log(`you are listening on localhost on port ${PORT}`);
+  console.log(
+    `Express server is running in ${process.env.NODE_ENV} and listening on port: ${PORT}`
+  );
 });
