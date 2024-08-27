8457762bb84bd35aa438224ee1c29e90ffeeac91
Eric Humphries <32630651+letItBezhumphries@users.noreply.github.com>
2024-08-27 12:16:27 -0700

    Update AbilitiesForm.jsx

--- frontend/src/screens/CharacterBuilderScreens/ChooseAbilities/AbilitiesForm.jsx
+++ frontend/src/screens/CharacterBuilderScreens/ChooseAbilities/AbilitiesForm.jsx
@@ -1,14 +1,38 @@
-import React from 'react';
-import { useState } from 'react';
+import React, { useEffect } from 'react';
+import { useState, useRef } from 'react';
 import { Form, Button } from 'react-bootstrap';
 import FormContainer from '../../../components/FormContainer';
 import './AbilitiesForm.css';
 
 const AbilitiesForm = () => {
+  const selectorRef = useRef();
+  const [currentSelectValue, setCurrentSelectValue] = useState(0);
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [strength, setStrength] = useState(0);
+  const [constitution, setConstitution] = useState(0);
+  const [intelligence, setIntelligence] = useState(0);
+  const [wisdom, setWisdom] = useState(0);
+  const [dexterity, setDexterity] = useState(0);
+  const [charisma, setCharisma] = useState(0);
+
   const abilityRolls = [8, 10, 12, 13, 14, 15];
   const [remainingAbilityRolls, setRemainingAbilityRolls] = useState(
     abilityRolls.slice()
   );
+
+  const [abilityScoresUsed, setAbilityScoresUsed] = useState([]);
+  let usedAbilityScores = abilityScoresUsed.length;
+
+  useEffect(() => {
+    console.log(
+      'the current selected value:',
+      currentSelectValue,
+      'is at index',
+      currentIndex
+    );
+    setRemainingAbilityRolls([...]);
+  }, [usedAbilityScores, currentIndex]);
+
   const abilities = [
     'Strength',
     'Constitution',
@@ -18,29 +42,60 @@ const AbilitiesForm = () => {
     'Charisma',
   ];
 
+  const setters = [
+    setStrength,
+    setConstitution,
+    setIntelligence,
+    setWisdom,
+    setDexterity,
+    setCharisma,
+  ];
+
+  useEffect(() => {
+    console.log(
+      'charisma:',
+      charisma,
+      strength,
+      dexterity,
+      intelligence,
+      wisdom,
+      constitution
+    );
+  }, [charisma, strength, dexterity, intelligence, wisdom, constitution]);
+
   const [abilityRollType, setAbilityRollType] = useState('standard');
-  const [strength, setStrength] = useState(0);
-  const [intelligence, setIntelligence] = useState(0);
-  const [constitution, setConstitution] = useState(0);
-  const [wisdom, setWisdom] = useState(0);
-  const [dexterity, setDexterity] = useState(0);
-  const [charisma, setCharisma] = useState(0);
 
   const handleRollTypeSelect = (e) => {
     console.log('the type of roll selected is :', e.target.value);
     setAbilityRollType(e.target.value);
   };
 
-  const handleAbilityScoreAdded = (e) => {
+  const handleAbilityScoreAdded = (e, index) => {
+    // e.preventDefault();
+    console.log('target element:', e.target, 'index of select el:', index);
+    // call the matching index from abilities to set the correct value for the ability
+    setters[index](parseInt(e.target.value));
+
+    console.log('selectRef:', selectorRef);
+    setCurrentSelectValue(parseInt(e.target.value));
+    setCurrentIndex(index);
+    setAbilityScoresUsed(parseInt(e.target.value));
+    // setCurrentIndex(remainingAbilityRolls.indexOf())
+    // console.log('in handleAbilityScoreAdded:', selectedRoll);
+    // let remainingRolls = remainingAbilityRolls.filter((roll) => {
+    //   return roll !== parseInt(selectedRoll);
+    // });
+
+    // console.log('remainingRolls:', remainingRolls);
+    // setRemainingAbilityRolls([...remainingRolls]);
+  };
+
+  const handleResettingRolls = function (e) {
     e.preventDefault();
-    console.log('target element:', e.target);
-    let selectedRoll = e.target.value;
-    console.log('in handleAbilityScoreAdded:', selectedRoll);
     let remainingRolls = remainingAbilityRolls.filter((roll) => {
-      return roll !== parseInt(selectedRoll);
+      return roll !== parseInt(e.target.value);
     });
 
-    console.log('remainingRolls:', remainingRolls);
     setRemainingAbilityRolls([...remainingRolls]);
   };
 
@@ -54,6 +109,7 @@ const AbilitiesForm = () => {
 
         <div style={{ marginBottom: '20px' }}>
           <select
+            ref={selectorRef}
             name='ability'
             id='ability'
             onChange={handleRollTypeSelect}
@@ -82,7 +138,10 @@ const AbilitiesForm = () => {
                     id={ability}
                     data-select-ability={ability}
                     className='select-ability'
-                    onChange={handleAbilityScoreAdded}
+                    onChange={(e) => {
+                      handleAbilityScoreAdded(e, idx);
+                      // handleResettingRolls(e);
+                    }}
                   >
                     <option value='--'>--</option>
                     {remainingAbilityRolls.map((ab, idx) => (
